251c5bd55fb2580c58ec0a93f1688d9032fec707
WillianNalbert <williannalbert@hotmail.com>
2023-10-10 23:29:07 -0300

    Inclusão das rotas get em evento

--- PROEVENTOS/BackEnd/src/ProEventos.API/ProEventos.API/Controllers/EventoController.cs
+++ PROEVENTOS/BackEnd/src/ProEventos.API/ProEventos.API/Controllers/EventoController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers;
@@ -7,21 +8,20 @@ namespace ProEventos.API.Controllers;
 [Route("api/[controller]")]
 public class EventoController : ControllerBase
 {
-    public EventoController(ILogger<EventoController> logger)
+    private readonly DataContext _context;
+    public EventoController(DataContext context)
     {
-       
+       _context = context;
     }
 
     [HttpGet]
     public IEnumerable<Evento> Get()
     {
-        return new Evento[]
-        {
-            new Evento(){
-                EventoId = 1,
-                Tema = "Teste",
-                Local = "Teste Local" 
-            }
-        };
+        return _context.Eventos;
+    }
+    [HttpGet("{id}")]
+    public Evento GetById(int id)
+    {
+        return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
     }
 }
