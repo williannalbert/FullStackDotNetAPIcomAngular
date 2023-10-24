02c3a2f7a0d858792430102fe0a1913250f935ee
WillianNalbert <williannalbert@hotmail.com>
2023-10-23 22:58:47 -0300

    Conclusão do filtro de eventos

--- PROEVENTOS/FrontEnd/ProEventosApp/src/app/eventos/eventos.component.ts
+++ PROEVENTOS/FrontEnd/ProEventosApp/src/app/eventos/eventos.component.ts
@@ -9,20 +9,46 @@ import { Component, OnInit } from '@angular/core';
 export class EventosComponent implements OnInit {
 
   public eventos: any = []; 
-  widthImg: number = 60
-  marginImg: number = 5
+  public eventosFiltrados: any = []; 
+  widthImg: number = 150;
+  marginImg: number = 5;
+  mostrarImagem = true;
+  private _filtroLista = '';
 
+  public get filtroLista(): string{
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string) : any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento : any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+      || evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+  }
   constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
     this.getEventos()
   }
 
+  exibirImagem(){
+    this.mostrarImagem = !this.mostrarImagem
+  }
+
   public getEventos(): void {
 
     this.http.get('https://localhost:7120/api/evento')
       .subscribe(
-        response => this.eventos = response,
+        response => {
+          this.eventos = response
+          this.eventosFiltrados = response
+        },
         error => console.log(error),
       );
 
