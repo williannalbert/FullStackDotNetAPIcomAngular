65db810a28dc41bdca3b682b0be66e8309ccf60f
WillianNalbert <williannalbert@hotmail.com>
2023-11-11 21:00:16 -0300

    ajustes no layout de components

--- PROEVENTOS/FrontEnd/ProEventosApp/src/app/shared/titulo/titulo.component.ts
+++ PROEVENTOS/FrontEnd/ProEventosApp/src/app/shared/titulo/titulo.component.ts
@@ -7,8 +7,10 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class TituloComponent implements OnInit {
   
-  @Input() titulo: string;
-
+  @Input() titulo: string = "";
+  @Input() iconClass = "fa fa-user";
+  @Input() subtitulo = "111";
+  @Input() btnListar = false;
   constructor() { }
 
   ngOnInit():void {
